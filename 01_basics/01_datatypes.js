82a5e70e52e4efc56f2b39ae3a5559e86bb5e106
Abhishek Garg <103814691+AbhishekGarg0507@users.noreply.github.com>
2024-04-13 09:23:02 +0000

    conversion of datatypes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -20,5 +20,36 @@ let isLoggedIn = false;
 
 
 // object
-console.log(typeof undefined);  // output => undeined
-console.log(typeof null); // output => object
+// console.log(typeof undefined);  // output => undeined
+// console.log(typeof null); // output => object
+
+
+//  conversion operations
+let score = "abhishek"
+
+// console.log(typeof score);
+// console.log( typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => gives NaN after converting
+// "true" => gives 1
+
+isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); 
+
+// 1 => true
+//  0 => false
+// "" => false
+// "abhi " => true
+// " " => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
